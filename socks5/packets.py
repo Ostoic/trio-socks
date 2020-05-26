32ac2bf942edf877d98be652265bcc19db326d0a
Ostoic <dinnertable@hotmail.ca>
2020-05-26 15:24:36 -0400

    Add socks 5 packets

--- socks5/packets.py
+++ socks5/packets.py
@@ -1,18 +1,73 @@
-from enum import Enum
+from construct import Enum, Struct, Byte, Const, this, PascalString, Flag, Switch, ExprAdapter, PrefixedArray, RawCopy, \
+	Short
+import ipaddress
 
-from generic_struct.parsers.StaticField import StaticFieldParser, UnsignedIntFormats
-from generic_struct.structs.GenericStruct import build_struct
-from generic_struct.parsers.DynamicSizeList import DynamicSizeListParser
+IPv4Address = ExprAdapter(RawCopy(Byte[4]),
+	decoder=lambda obj, ctx: ipaddress.IPv4Address(obj.data),
+	encoder=lambda obj, ctx: ipaddress.IPv4Address(obj.data).packed,
+)
 
-class AuthMethods(Enum):
-	no_auth = 0x0
-	gssap  = 0x1
-	username_password = 0x2
+IPv6Address = ExprAdapter(RawCopy(Byte[16]),
+	decoder=lambda obj, ctx: ipaddress.IPv6Address(obj.data),
+	encoder=lambda obj, ctx: ipaddress.IPv6Address(obj).packed,
+)
 
-@build_struct
-class ClientGreeting:
-	version = StaticFieldParser(UnsignedIntFormats.BYTE)
-	auth_methods = DynamicSizeListParser(
-		element_parser=FactoryFieldParser(Factory=AuthMethods),
-		size_parser=StaticFieldParser(UnsignedIntFormats.BYTE)
-	)
+AuthMethod = Enum(Byte,
+	no_auth = 0x0,
+	gssap  = 0x1,
+	username_password = 0x2,
+)
+
+auth_methods = AuthMethod
+
+ClientGreeting = Struct(
+	'version' / Const(5, Byte),
+	'auth_methods' / PrefixedArray(Byte, AuthMethod)
+)
+
+ServerChoice = Struct(
+	'version' / Const(5, Byte),
+	'auth_choice' / AuthMethod
+)
+
+ClientAuthRequest = Struct(
+	'version' / Byte,
+	'username' / PascalString(Byte, 'ascii')
+)
+
+ServerAuthResponse = Struct(
+	'version' / Byte,
+	'status' / Flag,
+)
+
+
+
+Socks5AddressType = Enum(Byte,
+	ipv4_address = 0x01,
+	domain_name = 0x03,
+	ipv6_address = 0x06,
+)
+
+# TODO: IPv6 parsing
+Socks5Address = Struct(
+	'type' / Socks5AddressType,
+	'address' / Switch(this.type, {
+		Socks5AddressType.ipv4_address: IPv4Address,
+		Socks5AddressType.domain_name: PascalString(Byte, 'ascii'),
+		# Socks5AddressType.ipv6_address: IPv6Address,
+	})
+)
+
+Socks5Command = Enum(Byte,
+	tcp_connect=0x01,
+	tcp_bind_port=0x02,
+    udp_bind_port=0x03,
+)
+
+ClientConnectionRequest = Struct(
+	'version' / Const(5, Byte),
+	'cmd' / Socks5Command,
+	'reserved' / Const(0, Byte),
+	'dest_address' / Socks5Address,
+	'dest_port' / Short
+)
