fbe32d9a7de49daae2edf24f74385d9bbdee72ea
Ostoic <dinnertable@hotmail.ca>
2020-05-28 23:20:08 -0400

    More setup.py stuff

--- setup.py
+++ setup.py
@@ -8,5 +8,15 @@ setup(
 	license='MIT',
 	author='Shaun Ostoic',
 	author_email='ostoic@uwindsor.ca',
-	description='trio-socks provides a trio.abc.HalfCloseableStream that routes its traffic through a SOCKS proxy server'
+	keywords=['socks5', 'trio-socks', 'trio'],
+	download_url='https://github.com/Ostoic/trio-socks/archive/v_001.tar.gz',
+	description='trio-socks provides a trio.abc.HalfCloseableStream that routes its traffic through a SOCKS proxy server',
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',  # Define that your audience are developers
+		'Topic :: Software Development :: Build Tools',
+		'License :: OSI Approved :: MIT License',  # Again, pick a license
+		'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+		'Programming Language :: Python :: 3.6',
+	],
 )
