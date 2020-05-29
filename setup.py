aa82a88de96d31426643947f67fd3a230dfc2354
Ostoic <dinnertable@hotmail.ca>
2020-05-28 23:33:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
 	author='Shaun Ostoic',
 	author_email='shaun.ostoic@gmail.ca',
 	keywords=['socks5', 'trio-socks', 'trio'],
-	download_url='https://github.com/Ostoic/trio_socks/archive/v0.1.0.tar.gz',
+	download_url='https://github.com/Ostoic/trio-socks/archive/v0.1.0.tar.gz',
 	description='trio-socks provides a trio.abc.HalfCloseableStream that routes its traffic through a SOCKS proxy server',
 	classifiers=[
 		'Development Status :: 3 - Alpha',
