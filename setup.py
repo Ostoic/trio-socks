34adb75887db0e80df9ddbf8baa59a3f0fab417b
Ostoic <dinnertable@hotmail.ca>
2020-05-28 23:52:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
 	name='trio-socks',
 	version='0.1.0',
 	packages=['trio_socks', 'trio_socks.socks5'],
+	install_requires=['trio', 'construct', 'ipaddress'],
 	url='https://github.com/Ostoic/trio-socks',
 	license='MIT',
 	author='Shaun Ostoic',
